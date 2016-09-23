9b0fdee49990702f92af0b9ba445fcc1c8cc7ef7
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-09-23 20:07:33 +0100

    Keep system's packages for speed

--- config/jenkins.sh
+++ config/jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf .venv
-virtualenv --python=python3 .venv
+virtualenv --system-site-packages --python=python3 .venv
 source .venv/bin/activate
 pip install -U pip wheel
 pip install -r requirements.txt --upgrade
