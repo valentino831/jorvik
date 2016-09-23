cc7ca54824bd65d029f6452ba09b1102ea00938e
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-09-23 19:14:51 +0100

    Random database name

--- config/jenkins.sh
+++ config/jenkins.sh
@@ -5,3 +5,6 @@ virtualenv --python=python3 .venv
 source .venv/bin/activate
 pip install -U pip wheel
 pip install -r requirements.txt --upgrade
+
+# Use random name for the new database -- used for testing
+sed "s/database = .*/database = jorvik$RANDOM/g" config/pgsql.cnf.sample > config/pgsql.cnf
