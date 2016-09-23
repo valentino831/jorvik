27405ec038be12b72741d01acb1468eee158d496
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-09-23 19:08:55 +0100

    Force recreation of .venv

--- config/jenkins.sh
+++ config/jenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+rm -rf .venv
 virtualenv --python=python3 .venv
 source .venv/bin/activate
-pip install pip wheel --force-reinstall
-pip install -r requirements.txt --force-reinstall
+pip install -U pip wheel
+pip install -r requirements.txt --upgrade
