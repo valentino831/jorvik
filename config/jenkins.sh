381dd04db70a996b1cf2f949ad4b58245a94c381
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-09-23 19:33:37 +0100

    Restore PSQL database

--- config/jenkins.sh
+++ config/jenkins.sh
@@ -6,5 +6,13 @@ source .venv/bin/activate
 pip install -U pip wheel
 pip install -r requirements.txt --upgrade
 
+DBNAME="jorvik_$RANDOM"
+TEST_DBNAME="test_$DBNAME"
+
 # Use random name for the new database -- used for testing
-sed "s/database = .*/database = jorvik$RANDOM/g" config/pgsql.cnf.sample > config/pgsql.cnf
+sed "s/database = .*/database = $DBNAME/g" config/pgsql.cnf.sample > config/pgsql.cnf
+
+psql -U postgres -c 'CREATE DATABASE $TEST_DBNAME;'
+psql -U postgres -c "CREATE EXTENSION postgis" -d $TEST_DBNAME
+psql -U postgres -c "CREATE EXTENSION postgis_topology" -d $TEST_DBNAME
+pg_restore -O -d $TEST_DBNAME ./base/test_jorvik.pgsql -j 4
